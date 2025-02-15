d53b919f97bd23bf279b85ca4b1a65bc95f2da1a
Koby Brackebusch <koby@goodcode.net>
2025-02-14 23:43:30 -0800

    initial postgres setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,47 @@
+import pg from 'pg';
+
+const config = {
+    host: 'brackebusch-web-db.postgres.database.azure.com',
+    // Do not hard code your username and password.
+    // Consider using Node environment variables.
+    user: 'redacted',
+    password: 'redacted',
+    database: 'postgres',
+    port: 5432,
+    ssl: true
+};
+
+const client = new pg.Client(config);
+
+client.connect(err => {
+    if (err) throw err;
+    else { queryDatabase(); }
+});
+
+function queryDatabase() {
+
+    console.log(`Running query to PostgreSQL server: ${config.host}`);
+
+    const query = 'SELECT * FROM posts;';
+
+    client.query(query)
+        .then(res => {
+            const rows = res.rows;
+
+            rows.map(row => {
+                console.log(`Read: ${JSON.stringify(row)}`);
+            });
+
+            process.exit();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
 export default function Home() {
+  queryDatabase();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       in progress
